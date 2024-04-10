15151db8ccfc1002351420948be7f6872e9aa761
warnnersinotti <warnner@talentacademy.com.br>
2024-04-10 20:28:21 -0300

    dynamic content test dynamic controls test

---
+++ cypress/e2e/web/14.dynamicContent.cy.ts
@@ -0,0 +1,25 @@
+describe('14-Teste validando texto com Dynamic Content', () => {
+    beforeEach(() => {
+        cy.visit(Cypress.env('BASE_URL'));
+        cy.get('ul > :nth-child(12) > a').should('contain.text', 'Dynamic Content').click();
+    });
+
+    it('Validando texto com Dynamic Content', () => {
+        cy.get('h3').should('contain.text', 'Dynamic Content');
+
+        cy.get('img').and('have.prop', 'naturalWidth').should('be.greaterThan', 0);
+
+        cy.get('#content > :nth-child(7) > .large-10')
+            .invoke('text')
+            .then((text) => {
+                const value1 = text;
+                cy.get(':nth-child(3) > a').click();
+                cy.get('#content > :nth-child(7) > .large-10')
+                    .invoke('text')
+                    .then((text) => {
+                        const value2 = text;
+                        expect(value1).not.be.eq(value2);
+                    });
+            });
+    });
+});
---
+++ cypress/e2e/web/15.dynamicControls.cy.ts
@@ -0,0 +1,38 @@
+import dadosFake from 'cypress/utils/dadosFake';
+
+describe('15-Teste validando texto com Dynamic Controls', () => {
+    const name: string = dadosFake.firstName;
+
+    beforeEach(() => {
+        cy.visit(Cypress.env('BASE_URL'));
+        cy.get('ul > :nth-child(13) > a').should('contain.text', 'Dynamic Controls').click();
+    });
+
+    it('Validando Textos em Dynamic Controls', () => {
+        cy.get('h4').should('contain.text', 'Dynamic Controls');
+        cy.get('.example > :nth-child(2)').should(
+            'include.text',
+            'This example demonstrates when elements (e.g., checkbox, input field, etc.) are changed asynchronously.'
+        );
+        cy.get('.example > :nth-child(3)').should('include.text', 'Remove/add');
+        cy.get('.example > :nth-child(8)').should('include.text', 'Enable/disable');
+    });
+
+    it('Removendo checkbox em Dynamic Controls', () => {
+        cy.get('h4').should('contain.text', 'Dynamic Controls');
+
+        cy.get('#checkbox > input').check();
+        cy.get('#checkbox-example > button').contains('Remove').click();
+        cy.get('#checkbox > input').should('not.exist');
+        cy.get('#checkbox-example > button').should('contain.text', 'Add');
+    });
+
+    it('Habilitando input text em Dynamic Controls', () => {
+        cy.get('h4').should('contain.text', 'Dynamic Controls');
+
+        cy.get('#input-example > input').should('be.disabled');
+        cy.get('#input-example > button').contains('Enable').click();
+        cy.get('#input-example > input').should('be.enabled').type(name);
+        cy.get('#message').should('contain.text', `It's enabled!`);
+    });
+});
